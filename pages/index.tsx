48ebfa45916fc19da76a91ad53a8a8a5df77f9a1
brandonmp <brandon@admitbrain.com>
2018-04-08 15:26:52 -0400

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import Absolute from '@components/Absolute';
+import Relative from '../components/Relative';
+
+export default () => (
+    <div>
+        <Absolute />
+        <Relative />
+    </div>
+);
